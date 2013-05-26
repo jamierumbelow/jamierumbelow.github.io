d9834f7ae19fc4208a3558b428493b8ba62b0806
Jamie Rumbelow <jamie@jamierumbelow.net>
2013-05-26 16:15:39 +0100

    Moved the header pattern generation out of the actual parsing routine and into the constructor

--- javascripts/markdown.js
+++ javascripts/markdown.js
@@ -1,17 +1,30 @@
 (function() {
   var Markdown, root;
   Markdown = (function() {
-    function Markdown() {}
+    function Markdown() {
+      this._generatePatterns();
+    }
     Markdown.prototype.toHTML = function(src) {
-      var hashes, lvl, pattern, _i, _len, _ref;
-      _ref = [6, 5, 4, 3, 2, 1];
-      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        lvl = _ref[_i];
-        hashes = Array(lvl + 1).join('#');
-        pattern = new RegExp("^" + hashes + "\\s?(.*)$", "gm");
+      var key, lvl, pattern, _len, _ref;
+      _ref = this.patterns;
+      for (key = 0, _len = _ref.length; key < _len; key++) {
+        pattern = _ref[key];
+        lvl = this.patterns.length - key;
         src = src.replace(pattern, "<h" + lvl + ">$1</h" + lvl + ">");
       }
       return src;
+    };
+    Markdown.prototype._generatePatterns = function() {
+      var hashes, lvl, _i, _len, _ref, _results;
+      this.patterns = [];
+      _ref = [6, 5, 4, 3, 2, 1];
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        lvl = _ref[_i];
+        hashes = Array(lvl + 1).join('#');
+        _results.push(this.patterns.push(new RegExp("^" + hashes + "\\s?(.*)$", "gm")));
+      }
+      return _results;
     };
     return Markdown;
   })();
