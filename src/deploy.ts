5799d2b8d8d5e0308adfecd20e3056d426b619ca
Jamie Rumbelow <jamie@jamierumbelow.net>
2025-05-11 10:56:53 +0100

    deploy 2025-05-11T09:56:53.229Z

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,7 +1,15 @@
 import { spawn } from "bun";
 
 export const deploy = async () => {
-  let proc = spawn(["git", "add", "."]);
+  let proc = spawn(["git", "status"]);
+  const text = await new Response(proc.stdout).text();
+  if (text.includes("nothing to commit")) {
+    console.log("Nothing to deploy");
+    return;
+  }
+  await proc.exited;
+
+  proc = spawn(["git", "add", "."]);
   await proc.exited;
   proc = spawn(["git", "commit", "-m", `deploy ${new Date().toISOString()}`]);
   await proc.exited;
