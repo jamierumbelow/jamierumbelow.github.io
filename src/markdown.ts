01a2c42a081f9333716be353e0bc2c1deb7311bd
Jamie Rumbelow <jamie@jamierumbelow.net>
2025-05-17 21:10:49 +0100

    deploy 2025-05-17T20:10:49.749Z

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,3 +1,16 @@
 import markdownit from "markdown-it";
 
 export const md = markdownit();
+
+// target="_blank" for all links
+
+const defaultRender =
+  md.renderer.rules.link_open ||
+  function (tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
+  };
+
+md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  tokens[idx].attrSet("target", "_blank");
+  return defaultRender(tokens, idx, options, env, self);
+};
